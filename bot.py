d5c9fa3ec6fad08831e40161d1aef775e9ee68d7
hawkab <hwakaba@ya.ru>
2017-02-21 18:29:48 +0300

    fix fetching errors

--- bot.py
+++ bot.py
@@ -38,14 +38,14 @@ def store_chat ( update ):
     chat_id = get_chat ( update )
     user_id = get_user ( update )
 
-    sql_exec ("INSERT OR REPLACE INTO CHAT (ID, TYPE, TITLE, FIRST_NAME, LAST_NAME) VALUES ((SELECT ID FROM CHAT WHERE ID = %d), %s , %s , %s , %s)"  % 
+    sql_exec ("INSERT OR REPLACE INTO CHAT (ID, TYPE, TITLE, FIRST_NAME, LAST_NAME) VALUES ((SELECT ID FROM CHAT WHERE ID = %d), '%s' , '%s' , '%s', '%s')"  % 
         ( chat_id 
             , update.message.type 
             , update.message.title
             , update.message.first_name 
             , update.message.last_name ))
 
-    sql_exec ("INSERT OR REPLACE INTO USER (ID, FIRST_NAME, LAST_NAME, USERNAME) VALUES ((SELECT ID FROM CHAT WHERE ID = %d), %s , %s , %s )"  % 
+    sql_exec ("INSERT OR REPLACE INTO USER (ID, FIRST_NAME, LAST_NAME, USERNAME) VALUES ((SELECT ID FROM CHAT WHERE ID = %d), '%s' , '%s' , '%s' )"  % 
             ( user_id 
             , update.message.from_user.first_name
             , update.message.from_user.last_name
