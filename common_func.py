622c8312a25805ace448963ef88315f13e1a9671
hawkab <hwakaba@ya.ru>
2017-02-22 10:29:38 +0300

    development process

--- common_func.py
+++ common_func.py
@@ -1,15 +1,12 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 import telegram, os, subprocess, sys, shlex
-import datetime, MySQLdb, logging, config
+import datetime, logging, config
 from time import sleep
 from subprocess import Popen, PIPE
 from telegram.ext import CommandHandler
 from imp import reload #модуль для перезагрузки (обновления) других модулей
 
-#bot = telegram.Bot(token = config.token)
-#Проверка бота
-#print(bot.getMe())
 from telegram import (ReplyKeyboardMarkup,ReplyKeyboardRemove)
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler)
 
