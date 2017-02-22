11c5678e155412bd1f1791aa3976b3e045462bd7
hawkab <hwakaba@ya.ru>
2017-02-22 09:54:18 +0300

    development process

--- config.py
+++ config.py
@@ -3,9 +3,3 @@
 token = '277254577:AAHSgYKjd3GT1dTuQCHCWpKLVh2QCiY_2OE'
 #Пользователи с доступом
 admin = ['155183865', '375624126']
-
-dir1 = '/var/lib/pgsql/9.4/data/base/'
-dir_backup = '/mnt/distr/backup/1c/' #путь к папке с файлом бэкапа у которого название формируется год_месяц_дата
-
-
-
